5f4b2f35f20f8c3814ac067dfd5bc4ba81f3a50e
Colin Alexander <colintalex@gmail.com>
2020-03-30 02:24:54 -0600

    Completes UserStory4

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,11 @@ class Trip < ApplicationRecord
 
   has_many :trip_trails
   has_many :trails, through: :trip_trails
+
+  def total_distance
+    total_dist = 0
+    trails.each {|trail| total_dist += trail.length}
+    total_dist
+  end
+
 end
--- spec/features/trip/show_spec.rb
+++ spec/features/trip/show_spec.rb
@@ -11,8 +11,28 @@ RSpec.describe "As a visitor on the hiking trips show page", type: :feature do
 
     within('.trails-all') do
       within("#trail-#{trail_1.id}") do
-        expect(page).to have_content(trip_1.name)
+        expect(page).to have_content(trail_1.name)
       end
+      within("#trail-#{trail_2.id}") do
+        expect(page).to have_content(trail_2.name)
+      end
+      within("#trail-#{trail_3.id}") do
+        expect(page).to have_content(trail_3.name)
+      end
+    end
+  end
+
+  it "I can see the total hiking distance of all trails on that trip" do
+    trip_1 = Trip.create(name:"Easter Weekend")
+    trail_1 = trip_1.trails.create(name:"Rocky Point", address: 'address', length:4)
+    trail_2 = trip_1.trails.create(name:"Shallow Gulf", address: 'address', length:4)
+    trail_3 = trip_1.trails.create(name:"Timberline", address: 'address', length:4)
+    trip_distance = trail_1.length + trail_2.length + trail_3.length
+
+    visit "/trips/#{trip_1.id}"
+
+    within('.total-distance-counter')do
+      expect(page).to have_content("Total trip distance: #{trip_distance}")
     end
   end
 end
